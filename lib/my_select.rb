919b6f149f682e79df9557ec9845887f35165090
Brooke Jones <bjones782@gmail.com>
2019-05-16 17:21:28 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,11 +1,15 @@
-def my_select(collection)
- i = 0 
- result = []
- while i < collection.size 
- if yield(collection[count]) == TRUE
-   result.push(collection[count])
+collection = [1, 2, 3, 4, 5]
+
+def my_select(array)
+  i = 0
+  select = []
+  while i < array.length
+    if yield(array[i])
+      select << array[i]
+    end
+    i+=1
   end
-  i += 1 
+  select
 end
-result
-end
+
+my_select(collection) { | num | num.even? }
